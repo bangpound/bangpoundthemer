a065f85dc56e352ae5aa08497028161ae7b309a5
Benjamin Doherty <bjd@pobox.com>
2010-02-17 13:00:52 -0600

    initial commit of bangpound themer module.

---
+++ mockups.js
@@ -0,0 +1,38 @@
+/*globals Drupal,$ */
+
+Drupal.behaviors.mockupOverlay = function (context) {
+  var $slider;
+  $slider = $('<div id="mockup-slider"></div>').css({
+    height: '100px',
+    width: '10px',
+    position: 'fixed'
+  });
+  $('#block-bangpoundthemer-mockups .item-list a', context).click(
+    function() {
+      var $anchor, $image;
+      $anchor = $(this);
+      $image = $('<img/>')
+        .attr('src', $anchor.attr('href'))
+        .appendTo('body')
+        .draggable({
+          zIndex: 10000
+        })
+        .css({
+          position: 'absolute',
+          top: 0,
+          left: 0
+        });
+      $slider
+        .prependTo('body')
+        .slider({
+          min: 0,
+          max: 100,
+          value: 60,
+          slide: function(event, ui) {
+            $image.css('opacity', (100 - ui.value) / 100);
+          }
+        });
+      return false;
+    }
+  );
+};
